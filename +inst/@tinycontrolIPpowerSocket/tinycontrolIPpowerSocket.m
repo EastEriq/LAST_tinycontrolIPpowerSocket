22f9c372a6da816353158892ae5100f335342915
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-02-02 12:46:32 +0200

    add MAC and Name properties

--- +inst/@tinycontrolIPpowerSocket/tinycontrolIPpowerSocket.m
+++ +inst/@tinycontrolIPpowerSocket/tinycontrolIPpowerSocket.m
@@ -2,13 +2,19 @@ classdef tinycontrolIPpowerSocket < obs.LAST_Handle
     
     properties
         Host='192.168.1.100'; % ip or hostname
-        User='admin';
-        Password='admin'
-        Outputs=false(1,6);  % power at each socket, off/on
+        User='admin'; % username for connecting, usually 'admin'
+        Password='admin' % password for the controlling user (should match what flashed in the device)
+        Outputs=false(1,6);  % power at each socket, false=OFF/true=ON
     end
     
     properties (Hidden)
         Options % weboptions() for web queries to the device, e.g. User, Password, Timeout
+    end
+    
+    % these properties are hidden because it takes time to retrieve them:
+    properties (Hidden,SetAccess=private)
+        MAC % mac address of the device
+        Name % name of the device, as flashed from the webby config
     end
     
     methods
@@ -57,7 +63,17 @@ classdef tinycontrolIPpowerSocket < obs.LAST_Handle
                 end
             end
         end
- 
+        
+        function m=get.MAC(T)
+            boardpage=webread(T.makeUrl('board.xml'),T.Options);
+            m=boardpage(strfind(boardpage,'<b6>')+4:strfind(boardpage,'</b6>')-1);
+        end
+        
+        function n=get.Name(T)
+            boardpage=webread(T.makeUrl('board.xml'),T.Options);
+            n=boardpage(strfind(boardpage,'<b7>')+4:strfind(boardpage,'</b7>')-1);
+        end
+        
     end
            
 end
