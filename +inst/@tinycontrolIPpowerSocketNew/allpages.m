e59184caf8a2a1c02004ad6ff9f083086ed2ad1e
PolishookDavid <david.polishook@weizmann.ac.il>
2023-04-24 09:58:53 +0000

    WIP class based on HttpClient

---
+++ +inst/@tinycontrolIPpowerSocketNew/allpages.m
@@ -0,0 +1,9 @@
+function xmlpages=allpages(T)
+% retrieve all xml pages from the IP power socket. To decide what to do
+%  with them if ever
+    pages={'st0.xml','st2.xml','s.xml','w.xml','sch.xml','board.xml','s_time.xml'};
+    xmlpages=cell(1,length(pages));
+    for i=1:length(pages)
+        xmlpages{i}=T.HttpClient.GET('PAGE',pages{i});
+    end
+end
--- +inst/@tinycontrolIPpowerSocketNew/tinycontrolIPpowerSocketNew.m
+++ +inst/@tinycontrolIPpowerSocketNew/tinycontrolIPpowerSocketNew.m
@@ -13,20 +13,22 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
     end
 
     properties (Hidden)
-        HttpClient; % the obs.api.HttpClient object responsible for transport
-        Host='192.168.1.100'; % ip or hostname
         User='admin'; % username for connecting, usually 'admin'
         Password='admin' % password for the controlling user (should match what flashed in the device)
     end
 
-    % these properties are hidden because it takes time to retrieve them:
     properties (Hidden,SetAccess=private)
+        HttpClient; % the obs.api.HttpClient object responsible for transport
+    end
+    
+    properties(Hidden, SetAccess=private,Description='api,must-be-connected')
+    % these properties are hidden because it takes time to retrieve them:
         MAC % mac address of the device
         Name % name of the device, as flashed from the webby config
     end
 
     properties (Hidden, Constant)
-        Timeout=2; % timeout for webreads, in seconds
+        Timeout=5; % timeout for webreads, in seconds
     end
 
     methods
@@ -42,6 +44,7 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
             else
                 id='';
             end
+            T.Id=id;
             % load configuration (including Host, [user, [password]])
             T.loadConfig(T.configFileName('create'))
             % fill initial status of untyped .Connected
@@ -68,10 +71,10 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
 
         function o=get.Outputs(T)
             try
-                resp = h.GET('PAGE','st0.xml');
+                resp = T.HttpClient.GET('PAGE','st0.xml');
                 o=false(1,6);
                 for i=1:6
-                    o(i)= resp(sprintf('out%d',i-1))=='1';
+                    o(i)= resp.(sprintf('out%d',i-1))==1;
                 end
                 T.LastError='';
             catch
@@ -85,8 +88,8 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
                 currentOutputs=T.Outputs;
                 for i=1:min(numel(outputs),6)
                     if outputs(i) ~= currentOutputs(i)
-                        webwrite(T.makeUrl(sprintf('outs.cgi?out%d=%d',i-1,outputs(i))),...
-                            T.Options);
+                        T.HttpClient.GET('PAGE',...
+                            sprintf('outs.cgi?out%d=%d',i-1,outputs(i)));
                     end
                 end
                 T.LastError='';
@@ -97,8 +100,7 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
 
         function m=get.MAC(T)
             try
-                boardpage=webread(T.makeUrl('board.xml'),T.Options);
-                m=boardpage(strfind(boardpage,'<b6>')+4:strfind(boardpage,'</b6>')-1);
+                m=T.HttpClient.GET('PAGE','board.xml').b6;
                 T.LastError='';
             catch
                 T.reportError('reading MAC of switch %s failed, offline?',T.Id);
@@ -108,10 +110,8 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
 
         function n=get.Name(T)
             try
-                boardpage=webread(T.makeUrl('board.xml'),T.Options);
-                n=boardpage(strfind(boardpage,'<b7>')+4:strfind(boardpage,'</b7>')-1);
                 % the retrieved name is 15 char long, right padded with spaces
-                n=strtrim(n);
+                n=strtrim(T.HttpClient.GET('PAGE','board.xml').b7);
                 T.LastError='';
             catch
                 T.reportError('reading name of switch %s failed, offline?',T.Id);
@@ -121,21 +121,17 @@ classdef tinycontrolIPpowerSocketNew < obs.LAST_Handle
 
         function b=get.Sensors(T)
             try
-                resp = webread(T.makeUrl('st0.xml'),T.Options);
-                ai=nan(1,9);
+                resp = T.HttpClient.GET('PAGE','st0.xml');
+                ai=nan(1,6);
                 for i=1:numel(ai)
-                    k1=strfind(resp,sprintf('<ia%d>',i-1))+5;
-                    k2=strfind(resp,sprintf('</ia%d>',i-1))-1;
-                    ai(i)=str2double(resp(k1:k2));
+                % data is apparently T/10, and -600 is what the webby displays as N/A 
+                    b.TemperatureSensors(i)=resp.(sprintf('ia%d',i))/10;
                 end
-                b.BoardTemperature=ai(1)/10;
-                % factor /10 guessed here, we'll see with real sensors
-                %  Apparently, -600 is what the webby displays as N/A 
-                b.TemperatureSensors=ai(2:7)/10;
-                b.VoltageSensor=ai(8)/100;
-                b.Vsupply=ai(9)/10;
+                b.TemperatureSensors(b.TemperatureSensors==-60)=NaN;
+                b.BoardTemperature=resp.ia0/10;
+                b.VoltageSensor=resp.ia7/100;
+                b.Vsupply=resp.ia8/10;
                 T.LastError='';
-
             catch
                 T.reportError('reading name of switch %s failed, offline?',T.Id);
                 b=[];
